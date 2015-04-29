768f64e6f7565be9ded5a5853a5b91916007de0f
Mikael Nortikyan <nortikyan99@gmail.com>
2015-04-29 12:10:27 -0700

    Quiz

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.dan').click(function(){
+
+	alert("Hey you clicked on me");
+	});
+});
+
+$(document).ready(function(){
+	$('#iwao').click(function(){
+	$('#iwao').hide();
+	});
+
+});
